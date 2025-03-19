d26335892a5ff5d7eea576943af35b88a37e60fb
AlecxLaishram <alecxlaishram@gmail.com>
2025-03-20 00:16:02 +0530

    Fix the error

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,49 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
 
+import { connectDb } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDb } from "./lib/db.js";
-import bodyParser from "body-parser";
-import {app, server} from "./lib/socket.js"
-import { fileURLToPath } from 'url'
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT : " + PORT);
-  connectDb()
+  console.log("server is running on PORT:" + PORT);
+  connectDb();
 });
